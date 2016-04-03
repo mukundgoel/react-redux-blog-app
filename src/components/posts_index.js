a580786f72c77b6fdf3cd5619293c8192b88ce71
“mgoel1” <mgoel@walmartlabs.com>
2016-04-02 17:04:52 -0700

    implementing get post functionality

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -12,6 +12,17 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <span className="pull-xs-right">{post.categories}</span>
+          <strong>{post.title}</strong>
+        </li>
+      )
+    })
+  }
+
   render() {
     return (
       <div>
@@ -20,10 +31,17 @@ class PostsIndex extends Component {
             Add a Post
           </Link>
         </div>
-        List of blog posts
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     )
   }
+}
+
+function mapStateToProps(state) {
+  return { posts: state.posts.all }
 }
 
 /*
@@ -35,4 +53,4 @@ we can replace this by { fetchPosts: fetchPosts } in connect function below
 also note { fetchPosts: fetchPosts } can be replaced by { fetchPosts }
 */
 
-export default connect(null, { fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
